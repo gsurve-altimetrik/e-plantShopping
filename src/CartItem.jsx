0215ee8b0b9fdc37d855e718a8a82953d10c27c3
Ganesh Govardhan Surve <gsurve@altimetrik.com>
2025-02-14 15:11:10 +0530

    Added e plannet implementation

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,34 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+      const { quantity, cost } = item;
+      total = total + (quantity * (parseFloat(cost.substring(1))));
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    alert('Functionality to be added for future reference');
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ name: item.name }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const { quantity, cost } = item;
+    return (quantity * (parseFloat(cost.substring(1))));
   };
 
   return (
